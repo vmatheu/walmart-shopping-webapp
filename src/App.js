1eb8e840032390ae07fe88a407a5a05b6d463d13
Victor Matheu <victor.matheu@latam.com>
2020-09-16 13:19:47 -0300

    added input component in SearchProduct

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import SearchProduct from 'modules/SearchProduct' 
 
 const App = () => (
-  <div className="container" />
+  <div className="container">
+    <SearchProduct />
+  </div>
   );
 
 export default App;
--- src/modules/SearchProduct/index.js
+++ src/modules/SearchProduct/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const SearchProduct = () => (
-  <div className="container" />
-    );
+  <div className="container">
+    <div className="row col-sm-12 col-md-5 ">
+      <p>Ingresar busqueda de Productos</p>
+    </div>
+    <div className="row col-sm-12 col-md-5">
+      <input
+        id="inputSearchById"
+        className="form-control"
+        style={{ width: '100%' }}
+      />
+    </div>
+    <div className="container" style={{ marginTop: '20px' }}>
+     
+    </div>
+  </div>
+);
 
 export default SearchProduct;
