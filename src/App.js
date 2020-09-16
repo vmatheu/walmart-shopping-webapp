df24c403f7110c08c557d9a1754ab7d95a198c8a
Victor Matheu <victor.matheu@latam.com>
2020-09-16 11:31:59 -0300

    the initial skeleton is added

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = props => (
+    <div class="container">
+    </div>
+  );
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'walmark-shopping-webapp.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    inline: true,
+    port: PORT,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/index.html`,
+    }),
+  ]
+}
