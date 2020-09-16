a164bb2901beff64ae9ba67416599b900535ad8d
Victor Matheu <victor.matheu@latam.com>
2020-09-16 18:15:31 -0300

    added change method for search input

--- src/modules/SearchProduct/_test/searchProduct.spec.js
+++ src/modules/SearchProduct/_test/searchProduct.spec.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
-import SearchProduct from '..';
+import productService from '../services/productService';
+import { SearchProduct, onChange } from '..';
+
+jest.mock('../services/productService');
 
 describe('<SearchProduct/>', () => {
+
   describe('With search product by id', () => {
     it('should have the same ui structure', () => {
       const wrapper = shallow(<SearchProduct />);
@@ -11,4 +15,21 @@ describe('<SearchProduct/>', () => {
       expect(tree).toMatchSnapshot();
     });
   });
+
+  describe('Onchange action', () => {
+    beforeEach(() => {
+      productService.getProductById = jest.fn();
+    });
+
+    it('should calling product service', () => {      
+      onChange("221");
+      expect(productService.getProductById).toHaveBeenCalled();
+    });
+
+    it('should not calling product service when search contains not alphanumeric characters', () => {      
+      onChange("21-");
+      expect(productService.getProductById).not.toHaveBeenCalled();
+    });
+  });
+
 });
--- src/modules/SearchProduct/index.js
+++ src/modules/SearchProduct/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import productService from './services/productService';
+import Logger from 'core/services/Logger';
 
-const SearchProduct = () => (
+export const onChange = async (search) => {
+  if((/^([0-9,a-z,A-Z]){1,}$/.test(search))) {
+    Logger.debbug(`search init for ${search}`);
+    const response = productService.getProductById(search);
+  }
+};
+
+export const SearchProduct = () => (
   <div className="container">
     <div className="row col-sm-12 col-md-5 ">
       <p>Ingresar busqueda de Productos</p>
     </div>
     <div className="row col-sm-12 col-md-5">
       <input
-        id="inputSearchById"
+        id="inputSearchId"
         className="form-control"
         style={{ width: '100%' }}
+        onChange={() => onChange(inputSearchId.value)}
       />
     </div>
-    <div className="container" style={{ marginTop: '20px' }}>
-     
-    </div>
+    <div className="container" style={{ marginTop: '20px' }} />
   </div>
 );
 
-export default SearchProduct;
+export default { SearchProduct, onChange };
