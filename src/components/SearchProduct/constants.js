053ad50b7e1d6c3af2cd9052d527597a636e7387
Victor Matheu <victor.matheu@latam.com>
2020-09-18 20:54:53 -0300

    added new url service

--- src/components/SearchProduct/constants.js
+++ src/components/SearchProduct/constants.js
@@ -1,4 +1,4 @@
-export const PRODUCT_SERVICE_ENDPOINT = 'https://walmark-api-shopping.herokuapp.com/product/filter/';
+export const PRODUCT_SERVICE_ENDPOINT = 'https://walmark-api-shopping.herokuapp.com/api/walmart/v1/product/filter/';
 
 export default {
   PRODUCT_SERVICE_ENDPOINT,
