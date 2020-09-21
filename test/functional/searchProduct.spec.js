bf29b2ea0d6781712801ed7c9c6d53875d4621a5
Victor Matheu <victor.matheu@latam.com>
2020-09-20 23:09:00 -0300

    adds wait time for functional test

--- test/functional/searchProduct.spec.js
+++ test/functional/searchProduct.spec.js
@@ -3,12 +3,14 @@ const VISIT_URL = 'http://localhost:3002';
 describe('App Functinal', () => {
   it('should be render search input component"', () => {
     cy.visit(VISIT_URL);
+    cy.wait(500);
     cy.get('input[id^="inputSearchId"]').should('exist');
   });
 
   it('should be product discount element when search is palindrome"', () => {
     cy.visit(VISIT_URL);
     cy.get('input[id^="inputSearchId"]').type(181);
+    cy.wait(500);
     cy.get('.card-header').should('exist');
     cy.get('.card-body').should('exist');
     cy.get('.card-header').should('exist');
@@ -18,6 +20,7 @@ describe('App Functinal', () => {
 
   it('should be product without discount element when search is not palindrome"', () => {
     cy.visit(VISIT_URL);
+    cy.wait(500);
     cy.get('input[id^="inputSearchId"]').type(12);
     cy.get('.card-header').should('exist');
     cy.get('.card-body').should('exist');
@@ -28,6 +31,7 @@ describe('App Functinal', () => {
 
   it('should be not render product when input search is not valid"', () => {
     cy.visit(VISIT_URL);
+    cy.wait(500);
     cy.get('input[id^="inputSearchId"]').type('aa');
     cy.get('.card-header').should('not.exist');
   });
